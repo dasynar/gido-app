f0a8b12e0236d617308de1bd09129a2ded066568
dasynar <m5mjdspskt@privaterelay.appleid.com>
2026-01-29 17:41:54 +0300

    Create page.tsx

---
+++ app/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '40px', textAlign: 'center', fontFamily: 'sans-serif' }}>
+      <h1>Gido Maskot Uygulaması Yayında!</h1>
+      <p>Dosyaları başarıyla yükledin.</p>
+      <img src="/mascot.jpg" style={{ width: '200px', marginTop: '20px' }} />
+    </div>
+  );
+}
