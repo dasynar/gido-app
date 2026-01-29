34931e0a080378ff497bb98eeaadb35d4d54dc88
dasynar <m5mjdspskt@privaterelay.appleid.com>
2026-01-29 17:53:38 +0300

    Delete app directory

--- app/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: '40px', textAlign: 'center', fontFamily: 'sans-serif' }}>
-      <h1>Gido Maskot Uygulaması Yayında!</h1>
-      <p>Dosyaları başarıyla yükledin.</p>
-      <img src="/mascot.jpg" style={{ width: '200px', marginTop: '20px' }} />
-    </div>
-  );
-}
